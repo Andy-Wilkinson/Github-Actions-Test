7c91f81dbb72dc78b7319b82ad0df5ff9a1be7f8
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2021-11-23 20:49:36 +0000

    Add package build step

---
+++ setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup, find_packages
+# from chemmltoolkit.__version__ import __version__
+
+# The directory containing this file
+here = pathlib.Path(__file__).parent
+
+# Read files for inclusion in the setup
+readme = (here / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="aw-githubactions-test",
+    # version=__version__,
+    description="A GitHub actions test package",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Andy-Wilkinson/Github-Actions-Test",
+    author="Andrew Wilkinson",
+    packages=find_packages(exclude=('tests', 'docs')),
+    include_package_data=True,
+)
