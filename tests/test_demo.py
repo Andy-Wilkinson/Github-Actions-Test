0ed40bc214962dd1ffbe75d6a14d798ec717641b
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2021-11-24 13:03:32 +0000

    Add unit tests

---
+++ tests/test_demo.py
@@ -0,0 +1,20 @@
+import pytest
+from code.demo import add, subtract
+
+
+class TestDemo(object):
+    @pytest.mark.parametrize("a,b,result", [
+        (1, 2, 3),
+        (2, 2, 4),
+        (3, 2, 5)
+    ])
+    def test_add(self, a: int, b: int, result: int):
+        assert add(a, b) == result
+
+    @pytest.mark.parametrize("a,b,result", [
+        (1, 2, -1),
+        (2, 2, 0),
+        (3, 2, 1)
+    ])
+    def test_subtract(self, a: int, b: int, result: int):
+        assert subtract(a, b) == result
